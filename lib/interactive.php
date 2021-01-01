7bea8e5678c4228a693f186496b7f590992319f9
liao2000 <liao2000@livemail.tw>
2021-01-01 13:20:03 +0800

    Upgrade mysqli code

--- lib/interactive.php
+++ lib/interactive.php
@@ -7,50 +7,45 @@ class Interactive{
 
     private static $_action = SELF::OTHER;
 
-    public static function conn(){
-        return (new Conn_main_db())->get_mysqli();
-    }
-
     public static function get_last_action(){
         return SELF::$_action;
     }
 
     private static function _like_auto($user_id, $serial, $type, $db_name){
-        $mysqli = SELF::conn();
-        $user_id = $mysqli->real_escape_string($user_id);
-        $serial  = $mysqli->real_escape_string($serial);
-        $type    = $mysqli->real_escape_string($type);
-
         // Did the user interactive with this article
         // i.e. does the database have record?
         // if no INSERT
         // else UPDATE / DELETE (Toggle)
-        $sql    = 'SELECT `TYPE` FROM `'.$db_name.'` WHERE `USER` = "'.$user_id.'" and `SERIAL` = "'.$serial.'"';
-        $result = $mysqli->query($sql);
-        if($result->num_rows == 0){
+        $db = new DB;
+        $res = $db->query('SELECT COUNT(TYPE) as num, TYPE FROM '.$db_name.'
+                           WHERE USER = ? and `SERIAL` = ?',
+                           $user_id, $serial);
+        $row = $res->fetch_assoc();
+
+        if($row['num'] == 0){
             if($type == 0) SELF::$_action = SELF::LIKE;
-            // 新增記錄
-            $values = 'VALUES ("'.$user_id.'", "'.$serial.'", "'.$type.'")';
-            $sql = 'INSERT INTO `'.$db_name.'`(`USER`, `SERIAL`, `TYPE`) '.$values;
-        }else{
-            $row = $result->fetch_assoc();
-            // TOGGLE
-            // 比如原本是 0 而 type 也是 0 那就刪除
-            // 原本是 0 而 type 是 1 那就更新
-            if($row['TYPE'] == $type){
-                if($type == 0) SELF::$_action = SELF::CANCEL_LIKE;
-                // DELETE
-                $sql = 'DELETE FROM `'.$db_name.'` WHERE `USER` = "'.$user_id.'" and `SERIAL` = "'.$serial.'"';
-            }else{
-                // UPDATE
-                $sql = 'UPDATE `'.$db_name.'` SET `TYPE` = "'.$type.'" WHERE `USER` = "'.$user_id.'" and `SERIAL` = "'.$serial.'"';
-            }
+            // add new record
+            $res = $db->query('INSERT INTO `'.$db_name.'`(`USER`, `SERIAL`, `TYPE`)
+                        VALUES (?, ?, ?)', $user_id, $serial, $type);
+            return;
         }
-        $mysqli->query($sql);
+
+        // TOGGLE 0->1 1->0
+        if($row['TYPE'] == $type){
+            if($type == 0) SELF::$_action = SELF::CANCEL_LIKE;
+            // DELETE
+            $db->query('DELETE FROM `'.$db_name.'`
+                        WHERE `USER` = ? and `SERIAL` = ?', $user_id, $serial);
+            return;
+        }
+
+        // UPDATE
+        $db->query('UPDATE `'.$db_name.'` SET `TYPE` = ?
+                    WHERE `USER` = ? and `SERIAL` = ?',
+                    $type, $user_id, $serial);
     }
 
-    // 更動 推─虛 (自動偵測)
-    // 回傳 推─虛 數量
+    // Audo update like-dislike & return the number of likes or dislikes
     public static function like_article_auto($user_id, $article_serial, $type=0){
         SELF::_like_auto($user_id, $article_serial, $type, 'article_interactive');
         return SELF::get_article_interactive_num($article_serial);
@@ -63,14 +58,11 @@ class Interactive{
 
     // return -1 if result 0
     private static function _get_user_interactive_with($user_id, $serial, $db_name){
-        $mysqli  = SELF::conn();
-        $user_id = $mysqli->real_escape_string($user_id);
-        $sql     = 'SELECT `TYPE` FROM `'.$db_name.'` WHERE `USER` = "'.$user_id.'" and `SERIAL` = "'.$serial.'"';
-        $result  = $mysqli->query($sql);
-        if($result->num_rows != 0){
-            return $result->fetch_assoc()['TYPE'];
-        }
-        return -1;
+        $db = new db;
+        $res = $db->query('SELECT TYPE FROM `'.$db_name.'`
+                           WHERE `USER` = ? and `SERIAL` = ?',
+                           $user_id, $serial);
+        return ($res->num_rows)? $res->fetch_assoc()['TYPE'] : -1;
     }
 
     // return -1 if this user do not interactive with this article
@@ -84,13 +76,13 @@ class Interactive{
     }
 
     private static function _get_interactive_num($serial, $db_name){
-        $mysqli = SELF::conn();
-        $ret    = array();
-        $sql    = 'SELECT `TYPE` FROM `'.$db_name.'` WHERE `TYPE` = 0 and `SERIAL` = "'.$serial.'"';
-        $ret['LIKE_NUM'] = $mysqli->query($sql)->num_rows;
-        $sql    = 'SELECT `TYPE` FROM `'.$db_name.'` WHERE `TYPE` = 1 and `SERIAL` = "'.$serial.'"';
-        $ret['DISLIKE_NUM'] = $mysqli->query($sql)->num_rows;
-        return $ret;
+        $db = new DB;
+        $res1 = $db->query('SELECT COUNT(TYPE) as num FROM `'.$db_name.'`
+                    WHERE TYPE = 0 and `SERIAL` = ?', $serial);
+        $res2 = $db->query('SELECT COUNT(TYPE) as num FROM `'.$db_name.'`
+                    WHERE TYPE = 1 and `SERIAL` = ?', $serial);
+        return array("LIKE_NUM" => $res1->fetch_assoc()['num'],
+                     "DISLIKE_NUM" => $res2->fetch_assoc()['num']);
     }
 
     public static function get_article_interactive_num($serial){
@@ -104,28 +96,26 @@ class Interactive{
     // return new condition a.k.a database num_rows (0 or 1)
     // return -1 if database error
     public static function star_article_auto($user_id, $serial){
-        $mysqli  = SELF::conn();
-        $flag    = SELF::has_user_starred_the_article($user_id, $serial);
-        $user_id = $mysqli->real_escape_string($user_id);
-        $serial  = $mysqli->real_escape_string($serial);
-
-        if($flag){
-            $sql = 'DELETE FROM `article_star` WHERE `USER` = "'.$user_id.'" and `SERIAL` = "'.$serial.'"';
+        $db = new DB;
+        if(SELF::has_user_starred_the_article($user_id, $serial)){
+            $res = $db->query('DELETE FROM `article_star`
+                               WHERE `USER` = ? and `SERIAL` = ?', $user_id, $serial);
             $nums = 0;
         }else{
-            $sql = 'INSERT INTO `article_star`(`USER`, `SERIAL`, `TIME`) VALUES ("'.$user_id.'", "'.$serial.'", '.time().')';
+            $res = $db->query('INSERT INTO `article_star`(`USER`, `SERIAL`, `TIME`)
+                               VALUES (?, ?, ?)', $user_id, $serial, time());
             $nums = 1;
         }
 
-        return ($mysqli->query($sql))? $nums : -1;
+        return ($res)? $nums : -1;
     }
 
     public static function has_user_starred_the_article($user_id, $serial){
-        $mysqli  = SELF::conn();
-        $user_id = $mysqli->real_escape_string($user_id);
-        $serial  = $mysqli->real_escape_string($serial);
-        $sql     = 'SELECT `USER` FROM `article_star` WHERE `USER` = "'.$user_id.'" and `SERIAL` = "'.$serial.'"';
-        return $mysqli->query($sql)->num_rows != 0;
+        $db = new DB;
+        $res = $db->query('SELECT COUNT(USER) as num FROM article_star
+                           WHERE USER = ? and `SERIAL` = ?',
+                           $user_id, $serial);
+        return $res->fetch_assoc()['num'] != 0;
     }
 }
 
